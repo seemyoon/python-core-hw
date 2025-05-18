2a500a59b7d2c12ce73a5564590cacf94d701495
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-05-18 22:14:32 +0300

    task 5 was done

--- hw5.py
+++ hw5.py
@@ -32,3 +32,39 @@
 #
 # with this list you should get the following result:
 # res = [1110, 1120, 1130, 1111, 1122, .......]
+
+data = [
+    [
+        {"id": 1110, "field": {}},
+        {"id": 1111, "field": {}},
+        {"id": 1112, "field": {}},
+        {"id": 1113, "field": {}},
+        {"id": 1114, "field": {}},
+        {"id": 1115, "field": {}},
+    ],
+    [
+        {"id": 1110, "field": {}},
+        {"id": 1120, "field": {}},
+        {"id": 1122, "field": {}},
+        {"id": 1123, "field": {}},
+        {"id": 1124, "field": {}},
+        {"id": 1125, "field": {}},
+
+    ],
+    [
+        {"id": 1130, "field": {}},
+        {"id": 1131, "field": {}},
+        {"id": 1122, "field": {}},
+        {"id": 1132, "field": {}},
+        {"id": 1133, "field": {}},
+
+    ]
+]
+
+res = []
+
+for subdata in data:
+    for value in subdata:
+        if value['id'] not in res: res.append(value['id'])
+
+print(res)
